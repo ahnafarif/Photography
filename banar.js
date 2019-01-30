8f2981bb329c00bec42fd5c7fba88da878822596
Ahnaf Arif <47189667+ahnafarif@users.noreply.github.com>
2019-01-31 02:32:09 +0600

    Add files via upload

---
+++ banar.js
@@ -0,0 +1,16 @@
+  $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if(scroll < 300){
+            $('.fixed-top').css('background', 'transparent');
+        } else{
+            $('.fixed-top').css('background', 'rgba(23, 162, 184, 0.9)');
+        }
+    });
+	//**portfolio**/
+function myFunction(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
